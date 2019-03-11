576f672569507f50b7d383bfa74b238d39828b32
389863294 <389863294@qq.com>
2019-03-11 14:15:28 +0800

    3月11日下午提交

---
+++ demo1/src/demo1/Demo1.java
@@ -0,0 +1,32 @@
+package demo1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		/*int a=5;
+		int b=10;
+		int c=18;
+		double q=6988.88;
+		double w=5999.99;
+		double e=4999.50;
+		System.out.print("品牌型号\t\t尺寸\t价格\t配置\t\t\t\t库存数");
+		System.out.printf("\nMacBookAir\t13.30\t%1.2f\ti5处理器4GB内存128G固态硬盘\t"+a,q);
+		System.out.printf("\nThinkpad450\t14.00\t%1.2f\ti5处理器4GB内存500G固态硬盘\t"+b,w);
+		System.out.printf("\nASUS-FL5800\t15.50\t%1.2f\ti7处理器4GB内存128G固态硬盘\t"+c,e);
+		System.out.print("\n总库存数:"+(a+b+c));
+		System.out.printf("\n库存商品总金额:%1.1f",(q*a+w*b+e*c));
+		*/
+		String a="你是傻逼";
+		if (a=="你是傻逼"){
+			
+				System.out.print("123");
+				
+			}
+		
+		}
+		
+
+	}
+
+
